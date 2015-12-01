ee385b7e7ceac8f0389bee52b123cce867367c51
Malik K <koljenovic@gmail.com>
2015-12-01 01:37:10 +0100

    Login bez sesija

--- site/router.php
+++ site/router.php
@@ -16,20 +16,50 @@ $urls = array(
         'rootUri' => $app->request()->getRootUri(),
         );
 
-$app->get('/', function () use ($twig, $urls) {
-    $ds = ldap_connect("192.168.1.134");
-    ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3);
-    ldap_set_option( $ds, LDAP_OPT_REFERRALS, 0 );
-    $r = ldap_bind($ds, "cn=admin,dc=db,dc=etf,dc=lab,dc=ba", "");
-    $sr = ldap_search($ds, "dc=db,dc=etf,dc=lab,dc=ba", "CN=mko*");
-    $info = ldap_get_entries($ds, $sr);
+function generateSalt() {
+    return uniqid(mt_rand(), true);
+}
 
-    echo print_r($info);
-    // echo $twig->render('@page/login.html', $urls);
+$app->get('/', function () use ($twig, $urls) {
+    
 });
 
-$app->post('/login/', function () use ($app, $twig, $urls) {
-    print_r($_POST);
+$app->get('/admin/', function () use ($twig, $urls) {
+    echo $twig->render('@page/login.html', $urls);
+});
+
+$app->post('/login/', function () {
+    try {
+        $ds = ldap_connect("192.168.1.134");
+        ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3);
+        ldap_set_option( $ds, LDAP_OPT_REFERRALS, 0 );
+        $user_data = explode('@', $_POST['email']);
+        $user = $user_data[0];
+        $pass = $_POST['pass'];
+        $r = ldap_bind($ds, "cn=$user,ou=users,dc=db,dc=etf,dc=lab,dc=ba", $pass);
+        $sr = ldap_search($ds, "dc=db,dc=etf,dc=lab,dc=ba", "cn=$user");
+        $info = ldap_get_entries($ds, $sr);
+        $r = [
+            'username' => $info[0]['cn'][0],
+            'name' => $info[0]['givenname'][0],
+            'surname' => $info[0]['sn'][0],
+            'session_key' => generateSalt()
+        ];
+        echo json_encode($r);
+    } catch (Exception $e) {
+        if ($e->getCode() != 2) {
+            $r = [
+                "error" => $e->getCode()
+            ];
+            echo json_encode($r);
+        } else {
+            echo json_encode([]);
+        }
+    }
+});
+
+$app->post('/logout/', function () {
+    // @TODO: invalidiraj sesiju
 });
 
 $app->run();
