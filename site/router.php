c0d90b8c492cfb6591b869d586489eb3ef653a1b
Malik K <koljenovic@gmail.com>
2015-12-09 17:50:12 +0100

    Povezujem front i back, unificirao login mgmt.

--- site/router.php
+++ site/router.php
@@ -19,45 +19,51 @@ $loader->addPath(realpath('../site/twig/template/part'), 'part');
 $loader->addPath(realpath('../site/twig/template/page'), 'page');
 $twig = new Twig_Environment($loader);
 
-$app = new \Slim\Slim();
-
-$urls = array(
-        'rootUri' => $app->request()->getRootUri(),
-        );
-
-require_once "vendor/autoload.php";
-
 $isDevMode = true;
-$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/class"), $isDevMode);
+$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__ . "/class"), $isDevMode);
 
 $conn = array(
     'url' => 'postgres://portal:test@localhost/portal',
 );
 
+$app = new \Slim\Slim();
 $em = EntityManager::create($conn, $config);
 
-function generateSalt() {
+$urls = array(
+    'rootUri' => $app->request()->getRootUri(),
+);
+
+$urls['ulogovan'] = is_ulogovan();
+
+function generateSalt()
+{
     return uniqid(mt_rand(), true);
 }
 
-$app->get('/', function () use ($twig, $urls, $em) {
+function is_ulogovan()
+{
+    global $em;
+    global $urls;
     if (array_key_exists('session', $_COOKIE)) {
         $c = explode(':', $_COOKIE['session']);
         $s = $em->find('Sesija', $c[1]);
-        if($s->getValidna()) {
-            echo '<h3>Ulogovan:</h3>';
-            echo '<pre>';
-            print_r($s);
-            echo '</pre>';
+        if ($s->getKljuc() == $c[0] && $s->getValidna()) {
+            $urls['korisnik'] = $s;
+            $urls['korisnik_string'] = var_export($s, true);
+            return true;
         } else {
             setcookie('session', '', time() - 60 * 60 * 24 * 30, $urls['rootUri'] . '/');
         }
     }
+    return false;
+}
+
+$app->get('/', function () use ($twig, $urls, $em) {
+    echo $twig->render('@page/landing.html', $urls);
 });
 
 $app->get('/login/', function () use ($twig, $urls) {
-    // @TODO: premjetiti u POST/login/ objediniti i refaktorisati da provjeri validnost sesije
-    if(!array_key_exists('session', $_COOKIE)) {
+    if (!$urls['ulogovan']) {
         echo $twig->render('@page/login.html', $urls);
     } else {
         header('Location: /');
@@ -66,76 +72,74 @@ $app->get('/login/', function () use ($twig, $urls) {
 });
 
 $app->post('/login/', function () use ($em, $urls) {
-    // @TODO objediniti sa GET/login
-    if(array_key_exists('session', $_COOKIE)) {
-        return;
-    }
-    try {
-        $ds = ldap_connect("localhost");
-        ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3);
-        ldap_set_option( $ds, LDAP_OPT_REFERRALS, 0 );
-        $user_data = explode('@', $_POST['email']);
-        $user = $user_data[0];
-        $pass = $_POST['pass'];
-        $r = ldap_bind($ds, "cn=$user,ou=users,dc=db,dc=etf,dc=lab,dc=ba", $pass);
-        $sr = ldap_search($ds, "dc=db,dc=etf,dc=lab,dc=ba", "cn=$user");
-        $info = ldap_get_entries($ds, $sr);
-        $r = [
-            'korisnik' => $info[0]['cn'][0],
-            'ime' => $info[0]['givenname'][0],
-            'prezime' => $info[0]['sn'][0],
-            'kljuc' => generateSalt(),
-            'lozinka' => $pass,
-            'rola' => '',
-        ];
-
-        $s = new Sesija();
-        $s->setKorisnik($r['korisnik']);
-        $s->setIme($r['ime']);
-        $s->setPrezime($r['prezime']);
-        $s->setKljuc($r['kljuc']);
-        $s->setLozinka($r['lozinka']);
-        $s->setRola($r['rola']);
-
-        $em->persist($s);
-        $em->flush();
-        setcookie('session', $r['kljuc'] . ':' . $s->getId(), time() + 60 * 60 * 24 * 30, $urls['rootUri'] . '/');
-
-        echo json_encode($r);
-    } catch (Exception $e) {
-        if ($e->getCode() != 2) {
+    if (!$urls['ulogovan']) {
+        try {
+            $ds = ldap_connect("localhost");
+            ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3);
+            ldap_set_option($ds, LDAP_OPT_REFERRALS, 0);
+            $user_data = explode('@', $_POST['email']);
+            $user = $user_data[0];
+            $pass = $_POST['pass'];
+            $r = ldap_bind($ds, "cn=$user,ou=users,dc=db,dc=etf,dc=lab,dc=ba", $pass);
+            $sr = ldap_search($ds, "dc=db,dc=etf,dc=lab,dc=ba", "cn=$user");
+            $info = ldap_get_entries($ds, $sr);
             $r = [
-                "error" => $e->getCode()
+                'korisnik' => $info[0]['cn'][0],
+                'ime' => $info[0]['givenname'][0],
+                'prezime' => $info[0]['sn'][0],
+                'kljuc' => generateSalt(),
+                'lozinka' => $pass,
+                'rola' => '',
             ];
+
+            $s = new Sesija();
+            $s->setKorisnik($r['korisnik']);
+            $s->setIme($r['ime']);
+            $s->setPrezime($r['prezime']);
+            $s->setKljuc($r['kljuc']);
+            $s->setLozinka($r['lozinka']);
+            $s->setRola($r['rola']);
+
+            $em->persist($s);
+            $em->flush();
+            setcookie('session', $r['kljuc'] . ':' . $s->getId(), time() + 60 * 60 * 24 * 30, $urls['rootUri'] . '/');
+            header('Location: /');
+            die();
 //            echo json_encode($r);
-            echo '<pre>';
-            echo $e->getTraceAsString();
-            echo '</pre>';
-//            print_r($e->getTrace());
-        } else {
-            echo json_encode([]);
+        } catch (Exception $e) {
+            if ($e->getCode() != 2) {
+                $r = [
+                    "error" => $e->getCode()
+                ];
+                //            echo json_encode($r);
+                echo '<pre>';
+                echo $e->getTraceAsString();
+                echo '</pre>';
+                //            print_r($e->getTrace());
+            } else {
+                echo json_encode([]);
+            }
         }
     }
 });
 
-
-
 $app->get('/logout/', function () use ($em, $urls) {
-    if(array_key_exists('session', $_COOKIE)) {
+    if ($urls['ulogovan']) {
         $c = explode(':', $_COOKIE['session']);
         $s = $em->find('Sesija', $c[1]);
-        if ($s->getKljuc() == $c[0] && $s->getValidna()) {
-            $s->setValidna(false);
-            $s->setKrajDt(new DateTime("now"));
-            $em->merge($s);
-            $em->flush();
-            setcookie('session', '', time() - 60 * 60 * 24 * 30, $urls['rootUri'] . '/');
-        } else {
-            $r = [
-                "error" => 'Sesija nije validna.',
-            ];
-            echo json_encode($r);
-        }
+        $s->setValidna(false);
+        $s->setKrajDt(new DateTime("now"));
+        $em->merge($s);
+        $em->flush();
+        setcookie('session', '', time() - 60 * 60 * 24 * 30, $urls['rootUri'] . '/');
+    }
+    header('Location: /');
+    die();
+});
+
+$app->get('/media/', function () use ($twig, $em, $urls) {
+    if ($urls['ulogovan']) {
+        echo $twig->render('@page/media.html', $urls);
     }
 });
 
